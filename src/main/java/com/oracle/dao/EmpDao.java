3006db529517b963a16dfd8fe63ee8b6c882f808
939796364 <36691741+939796364@users.noreply.github.com>
2018-02-21 16:41:15 +0800

    Update EmpDao.java

--- src/main/java/com/oracle/dao/EmpDao.java
+++ src/main/java/com/oracle/dao/EmpDao.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.Param;
 import com.oracle.vo.Emp;
 
 public interface EmpDao {
-	//dddddqwq 
+	//dddddqwq我在github上修改了注释，简单的使用了git
 	public  Emp getEmp(@Param("empId")int empId);
 	public void save(Emp e);
 	public List<Emp> selectEmp(Emp emp);
