54bb2ab41b7fd5995a8f88bf2593cb1d3c5a9c9f
shell <939796364@qq.com>
2018-02-21 16:08:03 +0800

    second commit

--- src/main/java/com/oracle/dao/EmpDao.java
+++ src/main/java/com/oracle/dao/EmpDao.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Param;
 import com.oracle.vo.Emp;
 
 public interface EmpDao {
+	//dddd
 	public  Emp getEmp(@Param("empId")int empId);
 	public void save(Emp e);
 	public List<Emp> selectEmp(Emp emp);
